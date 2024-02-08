aac3052b19fb1bdb0ca77785021410673095f0f2
Arsene2005 <159429401+Arsene2005@users.noreply.github.com>
2024-02-08 11:52:34 +0000

    Create infosutilisateur.py

---
+++ infosutilisateur.py
@@ -0,0 +1,7 @@
+echo "# ATLgit" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Arsene2005/ATLgit.git
+git push -u origin main
